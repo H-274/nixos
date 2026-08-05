ab3b6e27ba0e2cba11ae40d792777f624ef3658b
H-274 <GitHub@jsmarier.com>
2026-08-05 04:59:56 -0400

    feat: less headache

--- modules/hosts/main/hardware-configuration.nix
+++ modules/hosts/main/hardware-configuration.nix
@@ -4,24 +4,5 @@
       (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
-    boot.initrd.availableKernelModules = [ "ata_piix" "ohci_pci" "ehci_pci" "ahci" "sd_mod" "sr_mod" ];
-    boot.initrd.kernelModules = [ ];
-    boot.kernelModules = [ ];
-    boot.extraModulePackages = [ ];
-
-    fileSystems."/" = {
-      device = "/dev/disk/by-uuid/53bd6388-324a-4418-b30f-e5e868b20ab1";
-      fsType = "ext4";
-    };
-
-    swapDevices = [
-      {
-        device = "/dev/disk/by-uuid/d7a0b13e-e635-42f7-b375-d69cd8f9034f";
-      }
-    ];
-
-    nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
-    virtualisation.virtualbox.guest.enable = true;
-  };
-
+    # Copy from setup
 }
