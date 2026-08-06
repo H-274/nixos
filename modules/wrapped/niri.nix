727f049959e2e44f37a48a0fa218cf3c87379ff3
H-274 <GitHub@jsmarier.com>
2026-08-06 00:34:25 -0400

    fix: niri and noctalia

--- modules/wrapped/niri.nix
+++ modules/wrapped/niri.nix
@@ -1,19 +1,27 @@
 { self, inputs, ... }: {
+  flake.nixosModules.niri = { pkgs, lib, ... }: {
+    programs.niri = {
+      enable = true;
+      package = self.packages.${pkgs.stdenv.hostPlatform.system}.niri;
+    };
+  };
+
   perSystem = { pkgs, ... }: {
-    packages.myNiri = inputs.wrapper-modules.wrappers.niri.wrap {
+    packages.niri = inputs.wrapper-modules.wrappers.niri.wrap {
 
       inherit pkgs;
       settings = {
         input.keyboard = {
-          xkb.layout = "ca"
+          xkb.layout = "ca";
         };
         
         layout.gaps = 5;
 
         binds = {
           "Alt+Return".spawn-sh = lib.getExe pkgs.kitty;
-          "Alt+Q".close-window = "";
-        }
+          "Alt+Q".close-window = { };
+          "Alt+S".spawn-sh = "${lib.getExe self'.packages.noctalia} ipc call launcher toggle";
+        };
       };
     };
   };
