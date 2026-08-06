1c74b9760e2b49603a50d3f48254772f09b022e6
H-274 <GitHub@jsmarier.com>
2026-08-06 04:30:56 -0400

    fix: flake.nix

--- flake.nix
+++ flake.nix
@@ -9,7 +9,7 @@
 
     catppuccin.url = "github:catppuccin/nix";
 
-    inputs.hyprland.url = "github:hyprwm/Hyprland";
+    hyprland.url = "github:hyprwm/Hyprland";
   };
 
   outputs = inputs: inputs.flake-parts.lib.mkFlake
