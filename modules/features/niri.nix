3c01878c6446a80ee0e61f86813caa961ab69119
H-274 <GitHub@jsmarier.com>
2026-08-05 23:32:55 -0400

    feat: moving niri and noctalia

--- modules/features/niri.nix
+++ modules/features/niri.nix
@@ -2,17 +2,6 @@
   flake.nixosModules.niri = { pkgs, lib, ... }: {
     programs.niri = {
       enable = true;
-      package = self.packages.${pkgs.stdenv.hostPlatform.system}.myNiri;
-    };
-
-    environment.systemPackages = [
-      pkgs.niri
-    ];
-  };
-
-  perSystem = { pkgs, lib, self', ... }: {
-    packages.myNiri = inputs.wrapper-modules.wrappers.niri.wrap {
-      inherit pkgs;
       settings = {
         spawn-at-startup = [
           (lib.getExe self'.packages.myNoctalia)
@@ -20,13 +9,13 @@
 
         xwayland-satellite.path = lib.getExe pkgs.xwayland-satellite;
 
-        input.keyboard.xkb.layout = "us,ua";
+        input.keyboard.xkb.layout = "ca";
 
         layout.gaps = 5;
 
         binds = {
           "Mod+Return".spawn-sh = lib.getExe pkgs.kitty;
-          "Mod+Q".close-window = null;
+          #"Mod+Q".close-window = null;
           "Mod+S".spawn-sh = "${lib.getExe self'.packages.myNoctalia} ipc call launcher toggle";
         };
       };
