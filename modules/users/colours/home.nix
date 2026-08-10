5dbba676d6893924c4c108459bb3f37ab4f85a4b
H-274 <GitHub@jsmarier.com>
2026-08-09 20:45:14 -0400

    fix: home.nix?

--- modules/users/colours/home.nix
+++ modules/users/colours/home.nix
@@ -8,14 +8,15 @@
       wayland.windowManager.hyprland = {
         enable = true; # enable Hyprland
 
-        #settings = {
-        #  on = {
-        #    _args = [
-        #      "hyprland.start"
-        #      (lib.generators.mkLuaInline "function()\n  hl.exec_cmd(\"${lib.getExe self.packages.${pkgs.stdenv.hostPlatform.system}.noctalia}\")\nend")
-        #    ];
-        #  };
-        #};
+        settings = {
+          on = {
+            # ${lib.getExe self.packages.${pkgs.stdenv.hostPlatform.system}.noctalia}\")
+            _args = [
+              "hyprland.start"
+              (lib.generators.mkLuaInline "function()\n hl.exec_cmd(\"cowsay test\")\nend")
+            ];
+          };
+        };
       };
 
       programs = {
