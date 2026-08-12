2ae8da111f0d8cd9bda608913b4a37960502e14d
H-274 <GitHub@jsmarier.com>
2026-08-12 19:18:43 -0400

    fix: syntax

--- modules/features/hyprland/hyprland.nix
+++ modules/features/hyprland/hyprland.nix
@@ -17,7 +17,7 @@
 
     programs.hyprland = { 
       enable = true;
-      override { # or inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland
+      override = {
         enableXWayland = true;  # whether to enable XWayland
         withSystemd = true;     # whether to build with systemd support
       };
