09840fb1658ab6b81c460eb773fc844a8577cfa5
H-274 <GitHub@jsmarier.com>
2026-08-12 16:46:22 -0400

    feat: add zsh

--- modules/users/colours/home.nix
+++ modules/users/colours/home.nix
@@ -23,6 +23,23 @@
 
       programs = {
         home-manager.enable = true;
+        zsh = {
+          enable = true;
+          enableCompletion = true;
+          autosuggestion.enable = true;
+          syntaxHighlighting.enable = true;
+
+          shellAliases = {
+            ll = "ls -l";
+            edit = "sudo -e";
+            update = "sudo nixos-rebuild switch";
+          };
+
+          history.size = 10000;
+          history.ignoreAllDups = true;
+          history.path = "$HOME/.zsh_history";
+          history.ignorePatterns = ["rm *" "pkill *" "cp *"];
+        };
         git.enable = true;
         gh.enable = true;
         firefox.enable = true;
