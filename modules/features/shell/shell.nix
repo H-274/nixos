397ecaa96ff89f408503715b96ebe50ff5e9ca62
H-274 <GitHub@jsmarier.com>
2026-08-12 20:05:53 -0400

    feat: added oh-my-posh and zsh

--- modules/features/shell/shell.nix
+++ modules/features/shell/shell.nix
@@ -2,7 +2,13 @@
   flake.nixosModules.shell = { pkgs, ... }: {
     environment.systemPackages = with pkgs; [
       oh-my-posh
+      zsh
       kitty
     ];
+
+    programs = {
+      zsh.enable = true;
+      zsh.prompInit = "eval \"$(oh-my-posh init zsh --config ${./oh-my-posh.toml})\";
+    };
   };
 }
