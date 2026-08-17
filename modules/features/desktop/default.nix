5d3381cd314b3a3d878ec765fac0149b8bb63bc3
H-274 <GitHub@jsmarier.com>
2026-08-17 17:59:29 -0400

    fix: update default.nix?

--- modules/features/desktop/default.nix
+++ modules/features/desktop/default.nix
@@ -1,16 +1,16 @@
-{ self,  ... }: {
-  flake.nixosModules.desktop = { lib, ... }:  {
+{
+  flake.nixosModules.desktop = { lib, config, ... }:  {
     include = [
       ./plasma.nix
       ./hyprland.nix
     ];
 
     boot.loader.systemd-boot.enable = true;
-    services.displayManager.sddm.enable = true;
+    config.services.displayManager.sddm.enable = true;
 
     # X11 window system
-    services.xserver.enable = true;
-    services.xserver.xkb = {
+    config.services.xserver.enable = true;
+    config.services.xserver.xkb = {
       layout = "ca";
       variant = "";
     };
