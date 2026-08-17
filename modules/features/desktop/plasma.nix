3817b8e99e1defabc2f4d2d6330de4c7a881fe75
H-274 <GitHub@jsmarier.com>
2026-08-17 18:02:45 -0400

    fix: plasma.nix

--- modules/features/desktop/plasma.nix
+++ modules/features/desktop/plasma.nix
@@ -1,7 +1,7 @@
-{ lib, config, services, ... }: {
+{ lib, config, ... }: {
   options.desktop.plasma.enable = lib.mkEnableOption;
 
-  config = lib.mkIf config.desktop.plasma.enable {
+  lib.mkIf config.desktop.plasma.enable {
     services.desktopManager.plasma6.enable = true;
   };
 }
