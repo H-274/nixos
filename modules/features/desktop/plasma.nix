06d4daf155cbaa08d977aefa3b91020cadb60864
H-274 <GitHub@jsmarier.com>
2026-08-17 20:08:06 -0400

    fix: plasma.nix maybe

--- modules/features/desktop/plasma.nix
+++ modules/features/desktop/plasma.nix
@@ -1,8 +1,12 @@
-{ lib, config, ... }: let in {
+{ lib, config, ... }: {
   flake.nixosModules.plasma = { lib, config, ... }: let
     cfg = config.desktop.plasma;
   in {
-    options.desktop.plasma.enable = lib.mkEnableOption "Enable Plasma desktop";
+    options.desktop.plasma.enable = lib.mkOption {
+      type = lib.types.bool;
+      default = false;
+      description = "Enable Plasma desktop";
+    };
 
     config = lib.mkIf cfg.enable {
       services.desktopManager.plasma6.enable = true;
