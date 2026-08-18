5d693e0fe69147b48f1e534ee794b9ed4eef4ce2
H-274 <GitHub@jsmarier.com>
2026-08-17 21:33:59 -0400

    fix: home-manager.nix

--- modules/features/home-manager.nix
+++ modules/features/home-manager.nix
@@ -1,5 +1,9 @@
 { inputs, ... }: {
   flake.nixosModules.home-manager = {
+    imports = [
+      home-manager.nixosModules.default
+    ];
+
     home-manager = {
       useGlobalPkgs = true;
       useUserPackages = true;
