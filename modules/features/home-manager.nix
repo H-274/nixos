dfbe93fd486be5dbc3fcbb1b5bd2ed79fd01bf3e
H-274 <GitHub@jsmarier.com>
2026-08-20 18:45:59 -0400

    fix: home-manager.nix

--- modules/features/home-manager.nix
+++ modules/features/home-manager.nix
@@ -5,7 +5,7 @@
     ];
 
     home-manager.useGlobalPkgs = true;
-    home-manager.useUserPkgs = true;
+    home-manager.useUserPackages = true;
     home-manager.extraSpecialArgs = { inherit inputs; };
   };
 }
